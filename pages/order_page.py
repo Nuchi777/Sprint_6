fc57e22715e42f6058ef94503fb4f92b6aa0d1fb
Evgeny Koloskov <evgeny.koloskov@mail.ru>
2023-10-12 13:50:10 +0300

    fix method fill_all_fields_person by rderage

--- pages/order_page.py
+++ pages/order_page.py
@@ -1,4 +1,5 @@
 import time
+import re
 
 from selenium.webdriver import Keys
 
@@ -6,21 +7,21 @@ from pages.base_page import BasePage
 from locators.locators import OrderPageLocators
 import datetime
 import random
+from faker import Faker
+
+faker = Faker('ru_RU')
 
 
 class OrderPage(BasePage):
     locators = OrderPageLocators()
 
     def fill_all_fields_person(self):
-        self.element_is_visible(self.locators.NAME).send_keys('Иван')
-        self.element_is_visible(self.locators.SURNAME).send_keys('Иванов')
-        self.element_is_visible(self.locators.DELIVERY_ADDRESS).send_keys('Балашиха, ул. Автозаводская, 5')
-        self.element_is_visible(self.locators.METRO_STATION_LIST).click()
+        self.element_is_visible(self.locators.NAME).send_keys(faker.first_name())
+        self.element_is_visible(self.locators.SURNAME).send_keys(faker.last_name())
+        self.element_is_visible(self.locators.DELIVERY_ADDRESS).send_keys(f"{faker.street_title()}, {faker.building_number()}")
+        self.element_is_visible(self.locators.METRO_STATION_LIST).send_keys(random.choice([' Новокосино', ' Бауманская', ' Таганская', ' Фили']))
         self.element_is_visible(self.locators.METRO_STATION).click()
-        self.element_is_visible(self.locators.TEL_NUMBER).send_keys('+7888888888')
-
-    def click_next_button(self):
-        self.find_element(self.locators.NEXT_BUTTON).click()
+        self.element_is_visible(self.locators.TEL_NUMBER).send_keys(f"+7{random.choice(['925', '916', '903', '965'])}{random.randint(1000000, 9999999)}")
 
     def fill_all_fields_rent(self):
         date_today = str(datetime.date.today())
@@ -34,6 +35,9 @@ class OrderPage(BasePage):
 
     def click_order_button(self):
         self.find_element(self.locators.ORDER_BUTTON).click()
+
+    def click_next_button(self):
+        self.find_element(self.locators.NEXT_BUTTON).click()
 
     def click_yes_button(self):
         self.element_is_visible(self.locators.YES_BUTTON).click()
