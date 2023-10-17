4152ce86ae3428d77e618fb0a46b87eb20076cfa
Evgeny Koloskov <evgeny.koloskov@mail.ru>
2023-10-17 20:50:53 +0300

    fix test test_accordian_content

--- tests/tests_main_page.py
+++ tests/tests_main_page.py
@@ -55,28 +55,32 @@ class TestMainPage:
     @allure.title('Проверка выпадающего списка в разделе «Вопросы о важном»')
     @pytest.mark.parametrize("title_locator, content_locator, content_text",
                              [[MainPage.locators.ACCORDIAN_TITLE_0,
-                              MainPage.locators.ACCORDIAN_CONTENT_0,
-                              "Сутки — 400 рублей. Оплата курьеру — наличными или картой."]])
+                               MainPage.locators.ACCORDIAN_CONTENT_0,
+                               "Сутки — 400 рублей. Оплата курьеру — наличными или картой."],
+                              [MainPage.locators.ACCORDIAN_TITLE_1,
+                               MainPage.locators.ACCORDIAN_CONTENT_1,
+                               "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."],
+                              [MainPage.locators.ACCORDIAN_TITLE_2,
+                               MainPage.locators.ACCORDIAN_CONTENT_2,
+                               "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."],
+                              [MainPage.locators.ACCORDIAN_TITLE_3,
+                               MainPage.locators.ACCORDIAN_CONTENT_3,
+                               "Только начиная с завтрашнего дня. Но скоро станем расторопнее."],
+                              [MainPage.locators.ACCORDIAN_TITLE_4,
+                               MainPage.locators.ACCORDIAN_CONTENT_4,
+                               "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."],
+                              [MainPage.locators.ACCORDIAN_TITLE_5,
+                               MainPage.locators.ACCORDIAN_CONTENT_5,
+                               "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."],
+                              [MainPage.locators.ACCORDIAN_TITLE_6,
+                               MainPage.locators.ACCORDIAN_CONTENT_6,
+                               "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."],
+                              [MainPage.locators.ACCORDIAN_TITLE_7,
+                               MainPage.locators.ACCORDIAN_CONTENT_7,
+                               "Да, обязательно. Всем самокатов! И Москве, и Московской области."]])
     def test_accordian_content(self, driver, title_locator, content_locator, content_text):
         main_page = MainPage(driver)
         main_page.open('https://qa-scooter.praktikum-services.ru/')
         main_page.click_accordian_title(title_locator)
         content_text_locator = main_page.check_accordian_content(content_locator)
         assert content_text_locator == content_text
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
